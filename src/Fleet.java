4822b1e3e00813cd8da0f92d8faaf9096bcd775a
flolanz <florian.lanz@uzh.ch>
2022-10-25 23:35:41 +0200

    Basic Fleet and Output function

--- src/Fleet.java
+++ src/Fleet.java
@@ -1,2 +1,36 @@
-public class Fleet {
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Fleet implements Iterable<Ship> {
+
+    private ArrayList<Ship> aShips = new ArrayList<>();
+    private ArrayList<Ship> sunkShips = new ArrayList<>();
+
+    public Fleet(int carrier, int battleship, int submarine, int patrolboat) {
+
+        for (int i = 0; i < carrier; i++) {
+            aShips.add(new Ship("Carrier", 6));
+        }
+
+        for (int i = 0; i < battleship; i++) {
+            aShips.add(new Ship("BattleShip", 4));
+        }
+
+        for (int i = 0; i < submarine; i++) {
+            aShips.add(new Ship("Submarine", 3));
+        }
+
+        for (int i = 0; i < patrolboat; i++) {
+            aShips.add(new Ship("PatrolBoat", 2));
+        }
+    }
+
+    public int size() {
+        return aShips.size();
+    }
+
+    @Override
+    public Iterator<Ship> iterator() {
+        return aShips.iterator();
+    }
 }
--- src/Output.java
+++ src/Output.java
@@ -1,2 +1,30 @@
 public class Output {
+
+    private Grid targetGrid;
+    private Grid oceanGrid;
+    private final String LETTERS = "  A B C D E F G H I J  ";
+    private final String SEPARATOR = " +-+-+-+-+-+-+-+-+-+-+ ";
+
+    public Output(Grid targetGrid, Grid oceanGrid) {
+        this.targetGrid = targetGrid;
+        this.oceanGrid = oceanGrid;
+    }
+
+    public void print() {
+        System.out.println("===== TARGET GRID =====");
+        printGrid(this.targetGrid);
+        System.out.println("=======================\n-----------------------\n");
+        System.out.println("===== OCEAN GRID =====");
+        printGrid(this.oceanGrid);
+    }
+
+    private void printGrid(Grid grid) {
+        System.out.println(LETTERS);
+        System.out.println(SEPARATOR);
+        for (int i = 0; i < 10; i++) {
+            System.out.println(); //What values do I get from the grid?
+        }
+        System.out.println(SEPARATOR);
+        System.out.println(LETTERS);
+    }
 }
