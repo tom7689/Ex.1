5c684870a79a3777ea6cfdbd48a2b978e3620e2a
Rafael <rafael.urech@uzh.ch>
2022-10-26 12:38:52 +0200

    Implemented Class of grid and added methods to ship class

--- src/Ship.java
+++ src/Ship.java
@@ -6,13 +6,51 @@ public class Ship {
     private final String name;
     private final int length;
     private Position position;
-    private Direction direction;
+    private Orientation orientation;
 
     public Ship(String name, int length) {
         this.name = name;
         this.length = length;
         this.position = null;
-        this.direction = null;
+        this.orientation = null;
     }
+    public Ship(String name, int length, Position position, Orientation direction) {
+        this.name = name;
+        this.length = length;
+        this.position = position;
+        this.orientation = orientation;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public Position getPosition() {
+        return position;
+    }
+
+    public void setPosition(Position position) {
+        this.position = position;
+    }
+
+    public Orientation getOrientation() {
+        return orientation;
+    }
+
+    public void setOrientation(Orientation orientation) {
+        this.orientation = orientation;
+    }
+
+    public String toGraphicLength() {
+        return ("" + Board.SHIP).repeat(length);
+    }
+
+    public String toString(){
+        return name + ";" + length; /*+ ";" + (position.toString()) + ";" + direction;*/
+    }
+
 
 }
