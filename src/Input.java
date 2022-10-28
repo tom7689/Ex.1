b3c1e0886ff2b7a2cfea8aa3891ccb1859f2c5d8
patrick9051 <patrick.schelling@uzh.ch>
2022-10-28 18:11:05 +0200

    updated Input.java, still needs more input validation

--- src/Input.java
+++ src/Input.java
@@ -4,41 +4,49 @@ import java.util.Scanner;
 public class Input {
     private final Scanner aInput = new Scanner(System.in);
 
-    public Position readPosition() {
+    public Position readPosition(String pLine) {
         while (true) {
-            String aLine = aInput.nextLine();
-            Position position = parsePosition(aLine);
+            Position aPosition = parsePosition(pLine);
 
-            if (position != null) {
-                return position;
+            if (aPosition != null) {
+                return aPosition;
             }
             System.out.println("This is not a valid position");
         }
     }
     public Position parsePosition(String pLine) {
-        String aLine = pLine.trim().toLowerCase();
-        if (aLine.length() != 3) {
-            throw new IllegalArgumentException();
-        }
-        char aLetter = aLine.charAt(0);
+        String aLine = pLine.toLowerCase();
 
+        char aLetter = aLine.charAt(0);
         if (aLetter > 'j' || aLetter < 'a')
-            throw new IllegalArgumentException();
+            System.out.println("Column coordinate is not valid");
+
         int aColumnIndex = aLetter - 'a';
 
-        char aNumber = aLine.charAt(2);
+        char aNumber = aLine.charAt(1);
         if (aNumber > '9' || aNumber < '0')
-            throw new IllegalArgumentException();
+            System.out.println("Row coordinate is not valid");
+
         int aRowIndex = aNumber - '1' + 1;
+
         return new Position(aColumnIndex, aRowIndex);
     }
-    public Position inputShipPosition(Ship pShip) {
-        System.out.print("Enter Start-Position:");
-        Position aStartPosition = readPosition();
-        System.out.print("Enter End-Position:");
-        Position aEndPosition = readPosition();
-        if (hasCorrectLength(pShip, aStartPosition, aEndPosition)) {
-            return aStartPosition;
+    public Ship inputShipPosition(Ship pShip) {
+        Ship aShip = pShip;
+        System.out.println("Place your " + aShip.getName() + " (start and end position, separated by a comma)");
+        String aLine = aInput.nextLine();
+        if (aLine.length() != 5) {
+            System.out.println("Ship coordinates are not valid");
+            System.out.println("Try again");
+
+        }
+        Position aStartPosition = readPosition(aLine.substring(0,2));
+
+        Position aEndPosition = readPosition(aLine.substring(3));
+        if (hasCorrectLength(aShip, aStartPosition, aEndPosition)) {
+            aShip.setPosition(aStartPosition);
+
+            return pShip;
         }
         throw new IllegalArgumentException();
     }
