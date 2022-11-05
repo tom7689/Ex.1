3cf5c61048e82735c7c5bb334e43f15ed5540b4c
patrick9051 <patrick.schelling@uzh.ch>
2022-11-05 16:04:46 +0100

    added comments

--- src/Fleet.java
+++ src/Fleet.java
@@ -1,11 +1,18 @@
 import java.util.ArrayList;
 import java.util.Iterator;
 
+/**
+ * holds the ships of the standard fleet.
+ */
 public class Fleet implements Iterable<Ship> {
 
     private final ArrayList<Ship> aShips;
     private final ArrayList<Ship> sunkShips = new ArrayList<>();
 
+    /**
+     *
+     * @param pShips the flyweight ship objects.
+     */
     public Fleet(ArrayList<Ship> pShips) {
         aShips = pShips;
     }
@@ -13,7 +20,17 @@ public class Fleet implements Iterable<Ship> {
     public int size() {
         return aShips.size();
     }
+
+    /**
+     *
+     * @return size of sunk ships to compare with the size of a players fleet.
+     */
     public int sizeSunk(){ return sunkShips.size();}
+
+    /**
+     *
+     * @return each ship one by one.
+     */
     public Ship get(int i){return aShips.get(i);}
     public void addSunkShip(Ship ship){
         sunkShips.add(ship);
