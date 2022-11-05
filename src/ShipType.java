ab48b515e48af656c3f60c3add9b415b6553f10f
patrick9051 <patrick.schelling@uzh.ch>
2022-11-05 16:45:34 +0100

    added comments

--- src/ShipType.java
+++ src/ShipType.java
@@ -1,18 +1,9 @@
-import java.util.HashMap;
-import java.util.Map;
-
+/**
+ * defines the different ship types as enums.
+ */
 public enum ShipType {CARRIER("Carrier",6, 'C', 1), BATTLESHIP("BattleShip",4, 'B', 0),
-    SUBMARINE("Submarine",3, 'S', 0), PATROLBOAT("PatrolBoat", 2,'P', 0);
+    SUBMARINE("Submarine",3, 'S', 3), PATROLBOAT("PatrolBoat", 2,'P', 0);
 
-    private static final Map<ShipType, String> SHIP_TYPE_STRING_HASH_MAP = new HashMap<>();
-    private static final Map<ShipType, Integer> SHIP_TYPE_LENGTH_HASH_MAP = new HashMap<>();
-
-    static {
-        for (ShipType s : values()) {
-            SHIP_TYPE_STRING_HASH_MAP.put(s, s.name);
-            SHIP_TYPE_LENGTH_HASH_MAP.put(s, s.length);
-        }
-    }
     public final String name;
     public final int length;
     public final char initial;
@@ -23,12 +14,5 @@ public enum ShipType {CARRIER("Carrier",6, 'C', 1), BATTLESHIP("BattleShip",4, '
         this.length = length;
         this.initial = initial;
         this.quantity = amount;
-    }
-
-    public static String getStringOfShipType(ShipType pShipType) {
-        return SHIP_TYPE_STRING_HASH_MAP.get(pShipType);
-    }
-    public static Integer getLengthOfShipType(ShipType pShipType) {
-        return SHIP_TYPE_LENGTH_HASH_MAP.get(pShipType);
     }
 }
