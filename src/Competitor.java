eb6be896a9e061a73b7a2da914d4c8ccc4c66594
patrick9051 <patrick.schelling@uzh.ch>
2022-11-05 16:42:56 +0100

    added comments

---
+++ src/Competitor.java
@@ -0,0 +1,9 @@
+/**
+ * Player and Computer implement the Competitor interface.
+ */
+public interface Competitor {
+    void place_ship(Ship s, Position p1, Position p2);
+    void place();
+    void shoot(Grid enemyGrid);
+    boolean win();
+}
--- src/Spieler.java
+++
@@ -1,6 +0,0 @@
-public interface Spieler {
-    void place_ship(Ship s,Position p1, Position p2);
-
-    void place();
-    boolean win();
-}
