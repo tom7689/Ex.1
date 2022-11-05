7f23b2f5082ebcc2315500f245d19a81a3c24e4e
patrick9051 <patrick.schelling@uzh.ch>
2022-11-05 15:18:50 +0100

    added comments

--- src/Input.java
+++ src/Input.java
@@ -1,8 +1,16 @@
 import java.util.Scanner;
 
+/**
+ * Creates Input objects from the user to build position objects.
+ */
 public class Input {
     private final Scanner aInput = new Scanner(System.in);
 
+    /**
+     *
+     * @param pLine A String with two chars.
+     * @return A new position object when the column and row inputs are valid.
+     */
     private Position parsePosition(String pLine) {
         String aLine = pLine.toLowerCase();
         assert aLine.length() == 2;
@@ -24,6 +32,11 @@ public class Input {
 
         return new Position(aColumnIndex, aRowIndex);
     }
+
+    /**
+     * Asks the user to input their ships with start and end position and validates the length.
+     * @param pShip ship of the Players fleet.
+     */
     public void inputShipPosition(Ship pShip) {
         System.out.println("Place your " + pShip.getName() + " of length "+pShip.getLength()+" (start and end position, separated by a comma)");
         while (true) {
@@ -41,11 +54,22 @@ public class Input {
         }
     }
 
+    /**
+     *
+     * @param pShip ship of the Players fleet.
+     * @param pStartPosition a valid start position from the user.
+     * @param pEndPosition a valid end position from the user.
+     * @return true when start and end position are valid for the ship.
+     */
     private boolean hasCorrectLength(Ship pShip, Position pStartPosition, Position pEndPosition) {
         return Math.abs(pStartPosition.getaColumnIndex() - pEndPosition.getaColumnIndex()) == pShip.getLength() - 1 ||
                 Math.abs(pStartPosition.getaRowIndex() - pEndPosition.getaRowIndex()) == pShip.getLength() - 1;
     }
 
+    /**
+     * Asks the user to enter a shot to the target grid.
+     * @return a valid position on the grid.
+     */
     public Position enterShot() {
         System.out.println("Enter shot: ");
         while (true) {
