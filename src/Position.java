5f37f67786c149a1ced7ec850a5db7d1d9664040
patrick9051 <patrick.schelling@uzh.ch>
2022-11-05 14:54:11 +0100

    added comments

--- src/Position.java
+++ src/Position.java
@@ -5,13 +5,16 @@ import java.util.Objects;
  * as the input is created as well in this order. Instances of this class are immutable.
  */
 public final class Position {
-
     private final int aColumnIndex;
     private final int aRowIndex;
 
-
+    /**
+     *
+     * @param pColumn the column index of the grid from 0 to 9.
+     * @param pRow the row index of the grid from 0 to 9.
+     */
     public Position(int pColumn, int pRow) {
-        assert pRow >= 0 && pColumn >= 0;
+        assert 0 <= pRow && pRow <= 9 && 0 <= pColumn && pColumn <= 9;
         aColumnIndex = pColumn;
         aRowIndex = pRow;
     }
@@ -19,17 +22,27 @@ public final class Position {
     public int getaColumnIndex() {
         return aColumnIndex;
     }
-
     public int getaRowIndex() {
         return aRowIndex;
     }
+    /**
+     *
+     * @return true when pPosition in the same column.
+     */
     public boolean sameColumn(Position pPosition) {
         return aColumnIndex == pPosition.getaColumnIndex();
     }
+    /**
+     *
+     * @return true when pPosition in the same row.
+     */
     public boolean sameRow(Position pPosition) {
         return aRowIndex == pPosition.getaRowIndex();
     }
-
+    /**
+     *
+     * @return true when both the column and row index are the same.
+     */
     @Override
     public boolean equals(Object pPosition) {
         if (this == pPosition) return true;
