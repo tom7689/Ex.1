0039337739330e1db5bfe6bebde76d6585284560
patrick9051 <patrick.schelling@uzh.ch>
2022-11-07 08:40:47 +0100

    made place_ship private

--- src/Competitor.java
+++ src/Competitor.java
@@ -2,7 +2,6 @@
  * Player and Computer implement the Competitor interface.
  */
 public interface Competitor {
-    void place_ship(Ship s, Position p1, Position p2);
     void place();
     void shoot();
     boolean win();
